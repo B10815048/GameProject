6bf2420983dbe831da6ca548f5f23439b528024a
清億 許 <blackmirage2013@gmail.com>
2020-05-21 21:17:08 +0800

    14

--- Project1/Project1/Management(Move).cpp
+++ Project1/Project1/Management(Move).cpp
@@ -22,28 +22,28 @@ bool Management::Move(Creature& creature, std::string command)
 	{
 		if (command[i] == 'w')
 		{
-			if ((checkSpace({ tmp.x , tmp.y - 1 }) || checkDoor({ tmp.x , tmp.y - 1 })) && enemyOnPoint({ tmp.x, tmp.y - 1 }, creature.Camp) == 0)
+			if ((checkSpace({ tmp.x , tmp.y - 1 }) || (checkDoor({ tmp.x , tmp.y - 1 }) && creature.Camp== 0)) && enemyOnPoint({ tmp.x, tmp.y - 1 }, creature.Camp) == 0)
 				tmp.y--;
 			else if (creature.Camp == 0)
 				return false;
 		}
 		else if (command[i] == 's')
 		{
-			if ((checkSpace({ tmp.x , tmp.y + 1 }) || checkDoor({ tmp.x , tmp.y + 1 })) && enemyOnPoint({ tmp.x, tmp.y + 1 }, creature.Camp) == 0)
+			if ((checkSpace({ tmp.x , tmp.y + 1 }) || (checkDoor({ tmp.x , tmp.y - 1 }) && creature.Camp == 0)) && enemyOnPoint({ tmp.x, tmp.y + 1 }, creature.Camp) == 0)
 				tmp.y++;
 			else if (creature.Camp == 0)
 				return false;
 		}	
 		else if (command[i] == 'a')
 		{
-			if((checkSpace({ tmp.x - 1 , tmp.y }) || checkDoor({ tmp.x - 1 , tmp.y })) && enemyOnPoint({ tmp.x - 1, tmp.y }, creature.Camp) == 0)
+			if((checkSpace({ tmp.x - 1 , tmp.y }) || (checkDoor({ tmp.x , tmp.y - 1 }) && creature.Camp == 0)) && enemyOnPoint({ tmp.x - 1, tmp.y }, creature.Camp) == 0)
 				tmp.x--;
 			else if (creature.Camp == 0)
 				return false;
 		}
 		else if (command[i] == 'd')
 		{
-			if ((checkSpace({ tmp.x + 1, tmp.y }) || checkDoor({ tmp.x + 1, tmp.y })) && enemyOnPoint({ tmp.x + 1, tmp.y }, creature.Camp) == 0)
+			if ((checkSpace({ tmp.x + 1, tmp.y }) || (checkDoor({ tmp.x , tmp.y - 1 }) && creature.Camp == 0)) && enemyOnPoint({ tmp.x + 1, tmp.y }, creature.Camp) == 0)
 				tmp.x++;
 			else if (creature.Camp == 0)
 				return false;
